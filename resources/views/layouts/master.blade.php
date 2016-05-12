bb13dcfd8f884355bcd338a9b3c0fff3daafa405
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-12 07:43:55 -0500

    app_key

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,26 @@
     </head>
     <body>
         @section('sidebar')
-            This is the master sidebar.
+            <!--nav bar -->
+            <div id="wrapper">
+                <!-- Navigation -->
+                <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+                    <div class="row">
+                        <div class="col-xs-4">        
+                            <div class="navbar-header">
+                                <a class="navbar-brand" href="/">{{ isset($title) ? $title : "League Project" }}</a>
+                            </div>
+                            <!-- /.navbar-header -->
+                        </div>
+                        <div class="col-xs-4"> 
+                            <div class="navbar-right">
+                                Login Button Here
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+            <!--nav bar-->
         @show
 
         <div class="container">

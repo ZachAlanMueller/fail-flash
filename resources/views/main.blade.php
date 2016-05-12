4e85d4f1720bfca9a8cb4e74f312215552e5382b
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-12 08:01:13 -0500

    setup

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,12 +2,6 @@
 
 @section('title', 'Page Title')
 
-@section('sidebar')
-    @parent
-
-    <p>This is appended to the master sidebar.</p>
-@stop
-
 @section('content')
     <h1>Hello, world!</h1>
 @stop

03865e73d17848dcfa35634e6c471742bf910c48
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-12 07:52:38 -0500

    app_key

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -33,8 +33,8 @@
                             <!-- /.navbar-header -->
                         </div>
                         <div class="col-xs-6"> 
-                            <div class="navbar-right">
-                                Login Button Here
+                            <div class="navbar-header">
+                                <a class="navbar-brand text-xs-right" href="/auth/login">Login Here</a>
                             </div>
                         </div>
                     </div>

17e0259aa57f043dc391be08405cebe5d32fdbff
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-11 22:24:22 -0500

    app_key

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,20 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
+Route::get('/home', function() {
+	return view('welcome');
+});
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');

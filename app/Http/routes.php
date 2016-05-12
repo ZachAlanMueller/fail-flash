dbe4fdf22e31fd8ed9ec36cb8f6b157b1a5c3466
Zachary Mueller <zach.a.mueller@gmail.com>
2016-05-12 13:53:58 -0500

    setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,3 +25,6 @@ Route::get('auth/logout', 'Auth\AuthController@getLogout');
 // Registration routes...
 Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
+Route::auth();
+
+Route::get('/home', 'HomeController@index');

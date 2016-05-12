2c14d24bf30f638474ec13687e29864dfdbd7282
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-11 21:08:44 -0500

    app_key

--- config/app.php
+++ config/app.php
@@ -91,7 +91,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'x9DZ94svmv8r69Qs/vlizFCSKbMYr0UWpWaofIkl'),
+    'key' => env('APP_KEY', 'LeaEmsQ9RLig24CKJyotDX6Lt7s+L8I'),
 
     'cipher' => 'AES-256-CBC',
 

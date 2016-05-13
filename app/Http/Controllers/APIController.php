2e0001c94c6f03e4ab9d145eaf40eff9f9e6c321
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-13 11:02:47 -0500

    setup

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -3,10 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Redirect;
+use Input;
+use Auth;
+use stdClass;
+use DB;
 use App\Http\Requests;
 
 class APIController extends Controller
 {
-    //
+    public function databaseUpdates() {
+    	$input = Input::all('post');
+    	var_dump($input);
+    	die();
+
+
+
+
+
+
+
+    }
 }

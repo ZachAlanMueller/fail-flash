cd57de251630ade54bf7e431ce566ac7aa63af36
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-13 15:24:21 -0500

    setup

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -15,14 +15,14 @@ class APIController extends Controller
     public function databaseUpdates() {
     	$api_key = getAPI();
     	if(Input::has('champions')){
-    		$champions = json_decode(file_get_contents('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=image&api_key='.$api_key));
+    		$champions = json_decode(file_get_contents('https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData&api_key='.$api_key));
     		foreach($champions->data as $champion){
     			$checker = DB::table('champions')->where('id', $champion->id)->count();
     			if($checker < 1){
-    				DB::table('champions')->insert(array('id' => $champion->id, 'name' => $champion->name, 'key' => $champion->key, 'title' => $champion->title, 'img' => $champion->image->full, 'sprite' => $champions->image->sprite));
+    				DB::table('champions')->insert(array('id' => $champion->id, 'name' => $champion->name, 'key' => $champion->key, 'title' => $champion->title));
     			}
     			else{
-    				DB::table('champions')->where('id', $champion->id)->update(array('id' => $champion->id, 'name' => $champion->name, 'key' => $champion->key, 'title' => $champion->title, 'img' => $champion->image->full, 'sprite' => $champions->image->sprite));
+    				DB::table('champions')->where('id', $champion->id)->update(array('id' => $champion->id, 'name' => $champion->name, 'key' => $champion->key, 'title' => $champion->title));
     			}
     		}
     	}

8a25c8fa053400a507b698066dc690dea40b94d0
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-13 10:58:44 -0500

    setup

--- resources/views/adminUpdates.blade.php
+++ resources/views/adminUpdates.blade.php
@@ -14,7 +14,7 @@
 	    			<div class='row'>
 	    				<div class='col-xs-4'>
 	    					{{ Form::checkbox('champions', 'value') }}
-	    					{{ Form::label('champs, 'Update Champions Table')}}
+	    					Update Champions Table
 	    				</div>
 	    			</div>
 	    		</div>

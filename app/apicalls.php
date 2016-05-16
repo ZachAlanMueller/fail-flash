5a03e712fe4938bdedaed09cf58e09ffaafddf49
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-16 07:24:19 -0500

    setup

--- app/apicalls.php
+++ app/apicalls.php
@@ -5,6 +5,7 @@
 			$api_key = getAPI();
 			$summoner_name = trim($summoner_name);
 			$summoner_name = str_replace(' ', '', strtolower($summoner_name));
+			stall();
 			$info = json_decode(file_get_contents("https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/".$summoner_name."?api_key=".$api_key));
 			$info = $info->$summoner_name;
 			return $info;
@@ -16,9 +17,10 @@
 		
 	}
 
-	function API_SummonerID($summoner_id){ //https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Zaedonn?api_key=63d2786a-0782-49c7-a7fd-f1728e6c5071
+	function API_SummonerID($summoner_id){ //https://na.api.pvp.net/api/lol/na/v1.4/summoner/Zaedonn?api_key=63d2786a-0782-49c7-a7fd-f1728e6c5071
 		try{
 			$api_key = getAPI();
+			stall();
 			$info = json_decode(file_get_contents("https://na.api.pvp.net/api/lol/na/v1.4/summoner/".$summoner_id."?api_key=".$api_key));
 			$info = $info->$summoner_id;
 			return $info;
@@ -31,6 +33,28 @@
 	}
 
 	function API_Matchlist($summoner_id){ //https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/23703122?rankedQueues=TEAM_BUILDER_DRAFT_RANKED_5x5,RANKED_SOLO_5x5&seasons=SEASON2016&api_key=
-
+		try{
+			$api_key = getAPI();
+			stall();
+			$info = json_decode(file_get_contents("https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/".$summoner_id."?rankedQueues=TEAM_BUILDER_DRAFT_RANKED_5x5,RANKED_SOLO_5x5&seasons=SEASON2016&api_key=".$api_key));
+			$info = $info->matches;
+			return $info;
+		}
+		catch(Exception $e){
+			print("An Error Occured. It has been logged, please contact me and I will look into it.");
+			die();
+		}
 	}
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- app/helpers.php
+++ app/helpers.php
@@ -31,12 +31,44 @@
 		else{
 			DB::table('summoners')->where('id', $summonerInfo->id)->update(array('name' => $summonerInfo->name, 'id' => $summonerInfo->id, 'profile_icon_id' => $summonerInfo->profileIconId));
 		}
+
+		$summonerGames = API_Matchlist($id);
+		foreach($summonerGames as $game){
+			$count = DB::table('summoner_games')->where('game_id', $game->matchId)->count();
+			if($count < 1){
+				DB::table('summoner_games')->insert(array('id' => $id . "-" . $game->matchId, 'summoner_id' => $id, 'champ_id' => $game->champion, 'role' => $game->role, 'lane' => $game->lane, 'timestamp' => $game->timestamp, 'queue' => $game->queue, 'season' => $game->season, 'game_id' => $game->matchId));
+			}
+			else{
+				DB::table('summoner_games')->where('id', $id . '-' . $game->matchId)->update(array('id' => $id . "-" . $game->matchId, 'summoner_id' => $id, 'champ_id' => $game->champion, 'role' => $game->role, 'lane' => $game->lane, 'timestamp' => $game->timestamp, 'queue' => $game->queue, 'season' => $game->season, 'game_id' => $game->matchId));
+			}
+		}
+
+
+
+
+
+
 	}
 
+
+
+
+
+	// ------------- API CALL HELPERS ------------- \\
 	function getAPI(){
 		return '63d2786a-0782-49c7-a7fd-f1728e6c5071';
 	}
-
+	function stall(){
+		$state = DB::table('admin')->where('id', 1)->get();
+		$state = $state[0]->state;
+		if($state == 1){
+			sleep(2);
+			return;
+		}
+		else{
+			die();
+		}
+	}
 
 	function getDDragonVersion(){
 		$version = json_decode(file_get_contents("http://ddragon.leagueoflegends.com/realms/na.json"));

16146a3304e06d4a5bb56686919c9aa20fe6dd29
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-19 18:44:36 -0500

    Update Summoner working! Moving on to showing info

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,28 +4,29 @@
 
 @section('content')
     <div class="row">
-    	<div class="col-xs-4">
-    		<div class='panel panel-info'>
+    	<div class="col-xs-3">
+    		<div class='panel panel-info-primary'>
     			<div class='panel-heading'>
-    				Starting..
+    				Top Played Champions
     			</div>
     			<div class='panel-body'>
-    				<div class='row'>
-	    				<div class='col-xs-12'>
-	    					
-	    				</div>
-	    			</div>
+    				
 	    		</div>
     			<div class ='panel-footer'>
     				
     			</div>
 
     	</div>
-    	<div class="col-xs-4">
-
-    	</div>
-    	<div class="col-xs-4">
-
+    	<div class="col-xs-9">
+    		<div class='panel-heading'>
+    				Latest Game
+    			</div>
+    			<div class='panel-body'>
+    				
+	    		</div>
+    			<div class ='panel-footer'>
+    				
+    			</div>
     	</div>
     </div>
 @stop

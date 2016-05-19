775603ad2fdeffa7c44106a6fcba6f999b9e84b3
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-19 09:23:47 -0500

    test

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -37,6 +37,6 @@ Route::get('auth/register', 'Auth\AuthController@getRegister');
 Route::post('auth/register', 'Auth\AuthController@postRegister');
 Route::get('/login', 'Auth\AuthController@getLogin');
 Route::post('/login', 'Auth\AuthController@postLogin');
-Route::get('/logout', 'Auth\AuthController@getLogout');
+Route::get('/logout', 'Auth\AuthController@getLogout'); 
 Route::get('/register', 'Auth\AuthController@getRegister');
 Route::post('/register', 'Auth\AuthController@postRegister');

0a5f7a355ed05f78802e78dc21fee2786f285333
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-21 19:05:23 -0500

    new

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -48,15 +48,6 @@
 
 		});
     </script>
-    <style> 
-    	#recent-games{
-    		max-height:400px;
-    		overflow-y:scroll;
-    	}
-    </style>
-
-
-
 
 
     <div class="row row-eq-height">
@@ -93,7 +84,7 @@
     			</div>
     			<div class='panel-body'>
     				<div class="panel-group" id="accordion">
-    				<!-- @foreach($recentGames as $gameId => $game)
+    				@foreach($recentGames as $gameId => $game)
     				<div class="panel panel-default" data-toggle="collapse" data-parent="#accordion" href="#collapse-{{$gameId}}">
 	                    <div class="panel-heading">
 	                        <h4 class="panel-title">
@@ -107,7 +98,7 @@
 	                        </div>
 	                    </div>
 	                </div>
-    				@endforeach  -->
+    				@endforeach 
 	    		</div>
     		</div>
     	</div>

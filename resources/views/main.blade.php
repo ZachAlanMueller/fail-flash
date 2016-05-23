bc3cb08ef1c10ee1fe7635e4d7e8c03a15febc68
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-23 11:04:44 -0500

    posters

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,5 +3,14 @@
 @section('title', 'Fail Flash')
 
 @section('content')
-    <img src="{{$img_link}}" style="display: block;margin-left: auto;margin-right: auto;height: 600px; height:auto;">
+	<style>
+		#poster{
+			display: block;
+			margin-left: auto;
+			margin-right: auto;
+			height: 600px;
+			width: auto;
+		}
+	</style>
+    <img src="{{$img_link}}" id="poster">
 @stop

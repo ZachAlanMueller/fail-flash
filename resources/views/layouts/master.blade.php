3663a6aecc1b03c87137efdef1234d497f537bbc
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-23 16:40:44 -0500

    SummonerDisplay

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -162,11 +162,11 @@
 
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        <!-- Include all compiled plugins (below), or include individual files as needed -->
-        <script src="js/bootstrap.min.js"></script>
-
             <!-- jQuery -->
         <script src="../bower_components/jquery/dist/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
     </body>
 </html>

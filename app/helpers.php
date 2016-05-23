11708fe9b098036c4edc3922d680bea5d3de16cf
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-23 18:03:43 -0500

    SummonerDisplay

--- app/helpers.php
+++ app/helpers.php
@@ -66,7 +66,7 @@
 			if($region->queue == "RANKED_SOLO_5x5"){
 				foreach($region->entries as $user){
 					$count = DB::table('summoners')->where('id', $user->playerOrTeamId)->count();
-					$infoArray = array('name' => $user->playerOrTeamName, 'id' => $user->playerOrTeamId, 'league_points' => $user->leaguePoints, 'division' => $user->division, 'tier' => $region->tier, 'last_update' => date('m/d/Y h:i:s a', time()), 'tier_name' $region->name);
+					$infoArray = array('name' => $user->playerOrTeamName, 'id' => $user->playerOrTeamId, 'league_points' => $user->leaguePoints, 'division' => $user->division, 'tier' => $region->tier, 'last_update' => date('m/d/Y h:i:s a', time()), 'tier_name' => $region->name);
 					if($count == 0){
 						DB::table('summoners')->insert($infoArray);
 					}

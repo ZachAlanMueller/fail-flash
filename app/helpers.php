538d9e5289098ddc45c9b57bdde07399e371caaf
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-23 17:55:11 -0500

    SummonerDisplay

--- app/helpers.php
+++ app/helpers.php
@@ -80,12 +80,13 @@
             if($count == 0){
                 DB::table('games')->insert(array('id' => $game->matchId));
             }
-            $count = DB::table('summoner_games')->where('game_id', $game->matchId)->count();
+            $count = DB::table('summoner_games')->where('game_id', $game->matchId)->where('summoner_id', $summoner_id)->count();
+            $userInfo = array('id' => $summoner_id . "-" . $game->matchId, 'summoner_id' => $summoner_id, 'champ_id' => $game->champion, 'role' => $game->role, 'lane' => $game->lane, 'timestamp' => $game->timestamp, 'queue' => $game->queue, 'season' => $game->season, 'game_id' => $game->matchId)
             if($count < 1){
-                DB::table('summoner_games')->insert(array('id' => $id . "-" . $game->matchId, 'summoner_id' => $id, 'champ_id' => $game->champion, 'role' => $game->role, 'lane' => $game->lane, 'timestamp' => $game->timestamp, 'queue' => $game->queue, 'season' => $game->season, 'game_id' => $game->matchId));
+                DB::table('summoner_games')->insert($userInfo);
             }
             else{
-                DB::table('summoner_games')->where('id', $id . '-' . $game->matchId)->update(array('id' => $id . "-" . $game->matchId, 'summoner_id' => $id, 'champ_id' => $game->champion, 'role' => $game->role, 'lane' => $game->lane, 'timestamp' => $game->timestamp, 'queue' => $game->queue, 'season' => $game->season, 'game_id' => $game->matchId));
+                DB::table('summoner_games')->where('id', $summoner_id . '-' . $game->matchId)->update($userInfo);
             }
 		}
 

fbe5a1d777c9e1023361fc417dcb013f86d338cb
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-23 10:44:49 -0500

    posters

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -15,8 +15,6 @@ class MainController extends Controller
     public function landingPage()
     { 
         $files = glob('/home/forge/default/public/images/posters/*.*');
-        var_dump($files);
-        die();
         $file = array_rand($files);
         $img_link =  $files[$file];
         return view('main')

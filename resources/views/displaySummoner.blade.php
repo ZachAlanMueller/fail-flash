a8dbce7db4ce5754b174b0d11ad6443aa2927e61
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-23 22:24:29 -0500

    SummonerDisplay

--- resources/views/displaySummoner.blade.php
+++ resources/views/displaySummoner.blade.php
@@ -7,11 +7,11 @@
 		<div class="col-xs-6">
 			<div class="row">
 				<div class="col-xs-7">
-					<h1 class='text-center'>
+					<h1 class="text-center" style="margin-top:0px;font-family: Georgia, serif">
 						{{$summonerInfo->name}}
 					</h1>
 					<br>
-					
+
 				</div>
 				<div class="col-xs-5">
 				Refresh/Something

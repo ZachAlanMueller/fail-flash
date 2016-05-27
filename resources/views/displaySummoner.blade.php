570d9cf9ccf0d14d6162a0234245a6059cb32811
Zach Mueller -Macbook <zach.a.mueller@gmail.com>
2016-05-27 12:59:24 -0500

    last games

--- resources/views/displaySummoner.blade.php
+++ resources/views/displaySummoner.blade.php
@@ -47,8 +47,43 @@
         <!-- Tab panes -->
         <div class="tab-content">
             <div class="tab-pane fade in active" id="overview-pills">
-                <h4>Home Tab</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <div class="row">
+                	<div class="col-xs-6">
+                		@if(true)
+                			<canvas id="lastGame" width="auto" height="auto"></canvas>
+                			<script>
+                				$(document).ready(function() {
+	                				var ctx = ocument.getElementById('lastGame');
+	                				var lastGame = new Chart(ctx, {
+									    type: 'line',
+									    data: {
+									        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+									        datasets: [{
+									            label: '# of Votes',
+									            data: [12, 19, 3, 5, 2, 3]
+									        }]
+									    },
+									    options: {
+									        scales: {
+									            yAxes: [{
+									                ticks: {
+									                    beginAtZero:true
+									                }
+									            }]
+									        }
+									    }
+									});
+	                			});
+                			</script>
+                		@endif
+                	</div>
+                	<div class="col-xs-3">
+                		
+                	</div>
+                	<div class="col-xs-3">
+                		
+                	</div>
+                </div>
             </div>
             <div class="tab-pane fade" id="games-pills">
                 <h4>Profile Tab</h4>
